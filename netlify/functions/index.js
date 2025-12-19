1ffad32e83980d56055b11852424154dbbbd2725
Kyooo <keaunieting@gmail.com>
2025-12-20 06:50:49 +0800

    Testing the changes

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -14,6 +14,7 @@ dotenv.config();
 const OPEN_API_TOKEN_FILE = "/tmp/guesty-openapi-token.json";
 const BOOKING_TOKEN_FILE = "/tmp/guesty-booking-token.json";
 
+const apiBase = import.meta.env.VITE_API_BASE || "/.netlify/functions/index/api";
 
 /* =======================
    APP SETUP
