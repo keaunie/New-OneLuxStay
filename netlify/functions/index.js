725375d5022c4bfed1f1f4b3723e1597b5532bbd
Kyooo <keaunieting@gmail.com>
2026-01-23 11:37:17 -0800

    testing the new functions and nodes

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -45,7 +45,7 @@ app.use(
         allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
-app.options("*", cors());
+app.options("/*", cors());
 app.use(
     express.json({
         verify: (req, _res, buf) => {
