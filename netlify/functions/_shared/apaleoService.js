e83060bb6035c27c9de86c97ad5673e0d3f9e183
Kyooo <keaunieting@gmail.com>
2026-05-12 05:30:42 +0800

    Latest update on the APALEO DEVELOPMENT

--- netlify/functions/_shared/apaleoService.js
+++ netlify/functions/_shared/apaleoService.js
@@ -60,7 +60,7 @@ const getApaleoCredentials = () => {
   const scope = sanitizeString(
     process.env.APALEO_SCOPE ||
       process.env.APALEO_CLIENT_SCOPE ||
-      "reservations.read availability.read properties.read units.read",
+      "reservations.read availability.read setup.read",
     500,
   );
 
